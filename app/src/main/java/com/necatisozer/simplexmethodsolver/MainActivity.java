359105d105804967fab455cf3b931e3d031c9ff1
necatisozer <32370872+necatisozer@users.noreply.github.com>
2018-04-22 15:45:30 +0300

    Bug fixes

--- app/src/main/java/com/necatisozer/simplexmethodsolver/MainActivity.java
+++ app/src/main/java/com/necatisozer/simplexmethodsolver/MainActivity.java
@@ -19,105 +19,114 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    List<Equation> equations;
+  List<Equation> equations;
+  List<Integer> pivotConstants;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    FloatingActionButton fab = findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show();
+      }
+    });
 
-        equations = new ArrayList<>();
-        equations.add(new Equation(Arrays.asList(2.0, 3.0, 2.0, 1.0, 0.0, 0.0), 1000.0));
-        equations.add(new Equation(Arrays.asList(1.0, 1.0, 2.0, 0.0, 1.0, 0.0), 800.0));
-        equations.add(new Equation(Arrays.asList(-7.0, -8.0, -10.0, 0.0, 0.0, 1.0), 0.0));
-        printEquations();
-        calculate();
-        printEquations();
+    equations = new ArrayList<>();
+    pivotConstants = new ArrayList<>();
+
+    equations.add(new Equation(Arrays.asList(2.0, 3.0, 2.0, 1.0, 0.0, 0.0), 1000.0));
+    pivotConstants.add(3);
+    equations.add(new Equation(Arrays.asList(1.0, 1.0, 2.0, 0.0, 1.0, 0.0), 800.0));
+    pivotConstants.add(4);
+    equations.add(new Equation(Arrays.asList(-7.0, -8.0, -10.0, 0.0, 0.0, 1.0), 0.0));
+    printEquations();
+    calculate();
+    printEquations();
+  }
+
+  private void printEquations() {
+    String sEquation = " \n";
+
+    for (Equation equation : equations) {
+      sEquation += "\n";
+      sEquation +=
+          "Equation " + equations.indexOf(equation) + " = " + equation.getConstants().toString();
+      sEquation += " Result = " + equation.getResult();
     }
 
-    private void printEquations() {
-        String sEquation = "";
+    Log.d(getClass().getSimpleName(), sEquation);
+  }
 
-        for (Equation equation : equations) {
-            sEquation += "\n";
-            sEquation += "Equation " + equations.indexOf(equation) + " = " + equation.getConstants().toString();
+  private void calculate() {
+    while (ifContinueOrNot()) {
+      // Find pivot column index by comparing objective function constants
+      int numberOfEquation = equations.size();
+      List<Double> constants = equations.get(numberOfEquation - 1).getConstants();
+      int pivotColumnIndex = constants.indexOf(Collections.min(constants));
+
+      // Find row index of pivot by comparing ratios
+      int pivotRowIndex = 0;
+      double minRatio = Double.POSITIVE_INFINITY;
+
+      for (int i = 0; i < equations.size() - 1; i++) {
+        double ratio =
+            equations.get(i).getResult() / equations.get(i).getConstants().get(pivotColumnIndex);
+        if (ratio < minRatio) {
+          minRatio = ratio;
+          pivotRowIndex = i;
         }
+      }
 
-        Log.d(getClass().getSimpleName(), sEquation);
+      // Divide pivot's row by pivot
+      double pivot = equations.get(pivotRowIndex).getConstants().get(pivotColumnIndex);
+      equations.get(pivotRowIndex).divideBy(pivot);
+
+      // Divide other rows by their pivot column element
+      for (int i = 0; i < equations.size(); i++) {
+        if (i == pivotRowIndex) continue;
+        double pivotColumnElement = equations.get(i).getConstants().get(pivotColumnIndex);
+        equations.get(i)
+            .addMultipliedEquation(pivotColumnElement * (-1), equations.get(pivotRowIndex));
+      }
+      pivotConstants.set(pivotRowIndex, pivotColumnIndex);
+      printEquations();
     }
+  }
 
-    private void calculate() {
-        while (ifContinueOrNot()) {
-            // Find pivot column index by comparing objective function constants
-            int numberOfEquation = equations.size();
-            List<Double> constants = equations.get(numberOfEquation - 1).getConstants();
-            int pivotColumnIndex = constants.indexOf(Collections.min(constants));
-
-            // Find row index of pivot by comparing ratios
-            int pivotRowIndex = 0;
-            double minRatio = Double.POSITIVE_INFINITY;
-
-            for (Equation equation : equations) {
-                double ratio = equation.getResult() / equation.getConstants().get(pivotColumnIndex);
-                if (ratio < minRatio) {
-                    minRatio = ratio;
-                    pivotRowIndex = equations.indexOf(equation);
-                }
-            }
-
-            // Divide pivot's row by pivot
-            double pivot = equations.get(pivotRowIndex).getConstants().get(pivotColumnIndex);
-            equations.get(pivotRowIndex).divideBy(pivot);
-
-            // Divide other rows by their pivot column element
-            Equation pivotEquation = equations.get(pivotRowIndex);
-            for (Equation equation : equations) {
-                int index = equations.indexOf(equation);
-                if (index == pivotRowIndex) continue;
-                double pivotColumnElement = equation.getConstants().get(pivotColumnIndex);
-                equations.get(index).addMultipliedEquation(pivotColumnElement * (-1), pivotEquation);
-            }
-        }
+  private boolean ifContinueOrNot() {
+    int numberOfEquation = equations.size();
+    List<Double> constants = equations.get(numberOfEquation - 1).getConstants();
+    for (double constant : constants) {
+      if (constant < 0) return true;
     }
+    return false;
+  }
 
-    private boolean ifContinueOrNot() {
-        int numberOfEquation = equations.size();
-        List<Double> constants = equations.get(numberOfEquation - 1).getConstants();
-        for (double constant : constants) {
-            if (constant < 0) return true;
-        }
-        return false;
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    return super.onOptionsItemSelected(item);
+  }
 }
--- app/src/main/java/com/necatisozer/simplexmethodsolver/data/Equation.java
+++ app/src/main/java/com/necatisozer/simplexmethodsolver/data/Equation.java
@@ -5,61 +5,73 @@ import android.util.Log;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Equation {
-    private List<Double> constants;
-    private Double result;
+public class Equation implements Cloneable {
+  private List<Double> constants;
+  private Double result;
 
-    public Equation() {
-        constants = new ArrayList<>();
-        result = 0.0;
-    }
+  public Equation() {
+    constants = new ArrayList<>();
+    result = 0.0;
+  }
 
-    public Equation(List<Double> constants, Double result) {
-        this.constants = constants;
-        this.result = result;
-    }
+  public Equation(List<Double> constants, Double result) {
+    this.constants = constants;
+    this.result = result;
+  }
 
-    public List<Double> getConstants() {
-        return constants;
-    }
+  public List<Double> getConstants() {
+    return constants;
+  }
 
-    public void setConstants(List<Double> constants) {
-        this.constants = constants;
-    }
+  public void setConstants(List<Double> constants) {
+    this.constants = constants;
+  }
 
-    public Double getResult() {
-        return result;
-    }
+  public Double getResult() {
+    return result;
+  }
 
-    public void setResult(Double result) {
-        this.result = result;
-    }
+  public void setResult(Double result) {
+    this.result = result;
+  }
 
-    public void divideBy(double value) {
-        if (value != 0) {
-            for (int i = 0; i < constants.size(); i++) {
-                constants.set(i, constants.get(i) / value);
-                result /= value;
-            }
-        } else Log.d(getClass().getSimpleName(), "Divide by 0");
+  public void divideBy(double value) {
+    if (value != 0) {
+      for (int i = 0; i < constants.size(); i++) {
+        constants.set(i, constants.get(i) / value);
+      }
+      result /= value;
+    } else {
+      Log.d(getClass().getSimpleName(), "Divide by 0");
     }
+  }
 
-    public void multiply(double value) {
-        for (int i = 0; i < constants.size(); i++) {
-            constants.set(i, constants.get(i) * value);
-            result *= value;
-        }
+  public void multiply(double value) {
+    for (int i = 0; i < constants.size(); i++) {
+      constants.set(i, constants.get(i) * value);
     }
+    result *= value;
+  }
 
-    public void add(Equation equation) {
-        for (int i = 0; i < constants.size(); i++) {
-            constants.set(i, constants.get(i) + equation.getConstants().get(i));
-            result += equation.getResult();
-        }
+  public void add(Equation equation) {
+    for (int i = 0; i < constants.size(); i++) {
+      constants.set(i, constants.get(i) + equation.getConstants().get(i));
     }
+    result += equation.getResult();
+  }
 
-    public void addMultipliedEquation(double multiplyValue, Equation equation) {
-        equation.multiply(multiplyValue);
-        add(equation);
+  public void addMultipliedEquation(double multiplyValue, Equation equation) {
+    Equation mEquation = equation.clone();
+    mEquation.multiply(multiplyValue);
+    add(mEquation);
+  }
+
+  public Equation clone() {
+    List<Double> constants = new ArrayList<>();
+    for (double constant : this.constants) {
+      constants.add(constant);
     }
+    double result = this.result;
+    return new Equation(constants, result);
+  }
 }
